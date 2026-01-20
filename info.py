960690c3dffeb52e10fc815c7dbab490df788712
cartoonmakerhin-sudo <cartoonmakerhin@gmail.com>
2026-01-20 17:02:57 +0530

    Update info.py

--- info.py
+++ info.py
@@ -123,7 +123,7 @@ THREE_VERIFY_GAP = int(environ.get('THREE_VERIFY_GAP', "54000"))
 # ============================
 # Channel & Group Links Configuration
 # ============================
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+RXnEUdYTcNhmNjg1') # Group link for the bot
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+O8kEnZRbqBk4YzBl') # Group link for the bot
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/Jazzz0628') # Owner link for the bot
 UPDATE_CHNL_LNK = environ.get('UPDATE_CHNL_LNK', 'https://t.me/+s4wwf5daeWRmOGJl') # Update channel link for the bot
 
